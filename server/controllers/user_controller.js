cc140c68d4b8d3d3b157bf147cc20a19b1eb877d
xrr2016 <xiaoranran1993@163.com>
2017-03-29 23:11:56 +0800

    :ok_hand: edit user_controller

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -6,3 +6,43 @@ const moment = require('moment')
 const objectIdToTimeStamp = require('objectid-to-timestamp')
 const createToken = require('../moddleware/create_token')
 const checkToken = require('../moddleware/check_token')
+
+const userRegister = (req, res, next) => {
+  let user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    recheck: req.body.recheck,
+    token: createToken(req.body.email)
+  })
+  user.create_at = moment(objectIdToTimeStamp(user._id)).format('YYYY-MM-DD HH:mm:ss')
+  User.findById({ _id: user._id })
+      .then(user => {
+        if (user) {
+          res.json({info: '用户已存在!'})
+          next()
+        } else {
+          user.save(err => {
+            if (err) {
+              console.log(err)
+            }
+            res.json({success: '注册成功!'})
+            next()
+          })
+        }
+      })
+      .catch(err => console.log(err))
+}
+// 用户登录
+const userLogin = () => {}
+// 获取全部用户
+const getAllUser = () => {}
+// 删除一个用户
+const removeUser = () => {}
+
+module.exports = (router) => {
+  router.get('/allUser', getAllUser)
+  router.post('/userRegister', userRegister)
+  router.post('/userLogin', userLogin)
+  router.post('/removeUser', removeUser)
+}
