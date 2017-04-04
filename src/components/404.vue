6d23711d403c07473cee8445764f75e4cf889898
xrr2016 <xiaoranran1993@163.com>
2017-04-04 21:58:43 +0800

    :fire: edit

---
+++ src/components/404.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    <h1>404!</h1>
+    <h2>您访问的页面不存在!</h2>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <div>
+    <el-form>
+    </el-form>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
